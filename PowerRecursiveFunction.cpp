685119372c8d75bdd85383deea8cf20e38754cd1
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2018-10-27 16:34:30 -0700

    Add files via upload

---
+++ PowerRecursiveFunction.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+double power(double base, int exponent)
+{
+	return (exponent <= 0) ? 1 : (base * power(base, --exponent));
+}
+	
+int main()
+{
+	string statement,
+		againString;
+
+	double base = 0.0;
+
+	int exponent = 0;
+
+	char again;
+
+	do
+	{
+
+		cout << "Enter a base then an exponent seperate by space" << endl;
+
+		getline(cin, statement);
+
+		stringstream(statement) >> base >> exponent;
+
+		cout << power(base, exponent) << endl;
+
+		cout << "Would you like to power another number ? (Y / N)"
+			<< endl;
+
+		getline(cin, againString);
+
+		stringstream(againString) >> again;
+
+	} while (toupper(again) == 'Y');
+
+
+		return 0;
+	}
