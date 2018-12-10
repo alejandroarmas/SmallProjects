ccd6db5194224d99ff7b60c1341de021fc304153
Alejandro Armas <dlmhsarmasalejandro@gmail.com>
2018-12-09 19:54:47 -0800

    Update rangeCheckNoConditionalStructure.cpp

--- rangeCheckNoConditionalStructure.cpp
+++ rangeCheckNoConditionalStructure.cpp
@@ -5,18 +5,19 @@
 
 using namespace std;
 
-const int RANGE_MIN = 10;
-const int RANGE_MAX = 100;
+const int MIN = 10;
+const int MAX = 100;
 
 int main()
 {
 	string statement;
 	int number = 0;
 	vector<bool> vect(101, false);
+	
 	//vector of 101 falses. because index starts at 0
 
 	cout << "Please enter a set of numbers that are between 10 and 100." << endl;
-	cout << "==> ";
+	
 	getline(cin, statement);
 
 	stringstream ss(statement);
@@ -25,18 +26,19 @@ int main()
 	{
 		//if number is within range: set index at that position to true;
 		//if not within range it will endup setting true to index location 0 which we do not care about
-
-		vect.at ( (number >= RANGE_MIN && number <= RANGE_MAX) * number) = true;
+		vect.at( (number >= MIN && number <= MAX) * number) = true;
 	}
 
+	cout << "You list is: ";
 	// now all what we need is to print the numbers that we have (marked with true)
 	// and since we count sequentially, these number will be printed in order 
-	for (int i = RANGE_MIN; i < vect.size(); i++)
+	for (int i = MIN; i < vect.size(); i++)
 	{
 		// short-circuit-evaluation will not look at the right-side (hence cout) if left-side is false
 
-		(vect.at (i) != false )&&( cout << vect.at (i) << " ");
+		(vect.at(i)) && (cout << i << " ");
 	}
+	cout << endl;
 
 	return 0;
 }
